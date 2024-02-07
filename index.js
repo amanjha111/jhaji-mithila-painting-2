08f077fc3f30b5e419ff0d8da826221ed2c4f477
amanjha111 <144524490+amanjha111@users.noreply.github.com>
2024-02-07 19:23:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function addToCart(paintingId) {
+    console.log('Adding to cart:', paintingId);
+    // ... rest of the code
+
+    // Log the updated cart for testing
+    console.log('Updated cart:', cartItems);
+}
+
+function updateCartUI() {
+    console.log('Updating Cart UI');
+    // ... rest of the code
+}
+
